abdbdfdcbc4c9f24626d5bf66b734561eed2a5a5
Arseniy Samoylenko <godsallworld@gmail.com>
2024-03-18 15:42:22 +0300

    Upload

---
+++ bank_accounts.py
@@ -0,0 +1,40 @@
+from sqlalchemy import Column, Integer, Float
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sqlalchemy
+from sqlalchemy.exc import IntegrityError
+
+engine = create_engine("postgresql+psycopg2://postgres:12580@localhost/task_transaction", echo=True)
+Base = sqlalchemy.orm.declarative_base()
+
+
+class Account(Base):
+    __tablename__ = "bank_accounts"
+    account_id = Column(Integer(), primary_key=True)
+    balance = Column(Float(), nullable=False)
+
+
+# Base.metadata.create_all(bind=engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+try:
+    new_balance1 = Account(account_id=1, balance=100)
+    session.add(new_balance1)
+
+    new_balance2 = Account(account_id=2, balance=500)
+    session.add(new_balance2)
+
+    updated_acc = session.query(Account).filter_by(account_id=1).first()
+    updated_acc.balance = 150.0
+
+    delete_acc = session.query(Account).filter_by(account_id=2).first()
+    session.delete(delete_acc)
+
+    session.commit()
+
+except IntegrityError as i:
+    print("Ошибка при выполнении транзакции:", i)
+    session.rollback()
+
+session.close()
---
+++ orders.py
@@ -0,0 +1,35 @@
+from sqlalchemy import Column, Integer, Float
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sqlalchemy
+from sqlalchemy.exc import IntegrityError
+
+engine = create_engine("postgresql+psycopg2://postgres:12580@localhost/task_transaction")
+Base = sqlalchemy.orm.declarative_base()
+
+
+class Order(Base):
+    __tablename__ = "orders"
+    order_id = Column(Integer(), primary_key=True)
+    customer_id = Column(Integer(), nullable=False)
+    total_amount = Column(Float(), nullable=False)
+
+
+# Base.metadata.create_all(bind=engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+try:
+    new_order1 = Order(customer_id=12, total_amount=500)
+    session.add(new_order1)
+
+    new_order2 = Order(total_amount=125)
+    session.add(new_order2)
+
+    session.commit()
+except IntegrityError as i:
+    print("Ошибка при вставке данных:", i)
+    session.rollback()
+
+session.close()
